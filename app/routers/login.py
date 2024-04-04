5be71a60bc4614a47e0e73bf55d97df90f806f29
Andrew Chapman <andrew.j.b.chapman@gmail.com>
2024-04-03 18:12:01 -0700

    added username to login

--- app/routers/login.py
+++ app/routers/login.py
@@ -18,12 +18,15 @@ router = APIRouter(prefix="/login", tags=["Authentication"])
 
 @router.post("", response_model=Token)
 def login(*, db: Session = Depends(get_db), login : OAuth2PasswordRequestForm = Depends()):
-    password = login.password
+    
     email = login.username 
     result = db.execute(select(models.User).where(models.User.email == email))
     user = result.scalars().first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="INVALID CREDENTIALS")
+       result = db.execute(select(models.User).where(models.User.username == email))
+       user = result.scalars().first()
+       if not user:
+           raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="INVALID CREDENTIALS")    
     if not utils.verify(login.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="INVALID CREDENTIALS")
     acces_token = oauth2.create_token(data={"user_id": user.id})
