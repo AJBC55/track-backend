c8abaf08b031f311f308a2bfbe4fd2165c759c18
Andrew Chapman <andrew.j.b.chapman@gmail.com>
2024-04-13 19:49:15 -0700

    changed the img post router to img put

--- app/routers/admin_events.py
+++ app/routers/admin_events.py
@@ -62,7 +62,7 @@ def delete_event(*, db: Session = Depends(get_db), id: int,  user: User = Depend
     
     
     
-@router.post("/img/{id}")
+@router.put("/img/{id}")
 def add_event_img(*, db: Session = Depends(get_db), id: int, img: UploadFile ):
     
     file = img.file.read()
